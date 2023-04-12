448324739670ed9119e3752db5f9175e07bc3ab9
Kaan <kenkaraa@gmail.com>
2023-04-12 18:52:55 -0400

    Adds components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-
 import React from "react";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 const App = () => (
   <BrowserRouter>
