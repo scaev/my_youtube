8e079285aae7fafadb8e0503d46c95174efd10d6
Kaan <kenkaraa@gmail.com>
2023-04-14 15:20:22 -0400

    Adds search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 
 import React from "react";
+import { SearchBar } from "./SearchBar";
 
 const Navbar = () => (
   <Stack
