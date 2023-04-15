db67b1c3fbeb036b1501e4c34cfbf4f7a89c8911
Kaan <kenkaraa@gmail.com>
2023-04-15 12:11:32 -0400

    Fetch rapidAPI

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,7 @@ const Feed = () => {
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
