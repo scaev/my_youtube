3339c9ef27929c709725a6b472ba2310cf32d001
Kaan <kenkaraa@gmail.com>
2023-04-15 11:33:47 -0400

    Adds sidebar and feed component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 
 import React from "react";
-import { SearchBar } from "./SearchBar";
+import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack
