52e7eaadad7feb111f74a24e0484325eefee4446
Kaan <kenkaraa@gmail.com>
2023-04-15 14:39:10 -0400

    Adds responsive design & Fix card's size

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -18,15 +18,41 @@ const VideoCard = ({
 }) => {
   return (
     <Card
-
+      sx={{
+        width: { xs: "100%", sm: "358px", md: "320px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
+          //if there is an error, returns undefined
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width:358, height: 180 }}
+          sx={{ width: 358, height: 180 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircleIcon
+              sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
