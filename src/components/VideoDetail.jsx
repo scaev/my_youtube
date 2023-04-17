7d16ebb19c2be187a7f518111d46262e02b45406
Kaan <kenkaraa@gmail.com>
2023-04-17 15:56:22 -0400

    Adds react-player to videoDetails component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
 
-export default VideoDetail
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
